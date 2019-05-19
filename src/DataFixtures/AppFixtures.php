d82be487d05ece498c72a698e783a4059c5f0fdf
perriercreatif <perriercreatif@gmail.com>
2019-05-19 16:07:14 +0200

    new

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -13,6 +14,23 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
+
+        //Nous gérons les utilisateurs
+        $users = [];
+        for($i = 1; $i <= 10; $i++){
+            $user = new User();
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>')
+                ->setHash('password');
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        //Nous gerons les annonces
         for ($i = 1; $i <= 30; $i++){
             $ad = new Ad();
 
@@ -21,12 +39,15 @@ class AppFixtures extends Fixture
             $introduction = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
 
+            $user = $users[mt_rand(0, count($users)-1)];
+
             $ad->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 5));
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user);
 
             for ($j = 1; $j <= mt_rand(2, 5); $j++){
                 $image = new Image();
