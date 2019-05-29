eb4e994de4958f76e819400d9f01611282c5a2ca
perriercreatif <perriercreatif@gmail.com>
2019-05-29 22:41:52 +0200

    new

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +13,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function home(){
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
         return $this->render(
-            'home.html.twig'
+            'home.html.twig',
+            [
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
+            ]
         );
     }
 
